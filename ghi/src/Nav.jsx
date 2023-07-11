a3df5b694de2ac29f59aead3adcb6223fc95479b
Brad <bradcochi@gmail.com>
2023-07-11 17:25:53 -0600

    uodated Nav and apiSlice

--- ghi/src/Nav.jsx
+++ ghi/src/Nav.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useGetAccountQuery, useLogoutMutation } from "./app/apiSlice";
 
 const Nav = () => {
+  const { data: account, error, isLoading } = useGetAccountQuery();
+  const [logout, logoutResponse] = useLogoutMutation();
+
+  console.log({ account });
+
   return (
-
     <nav>
-        <div>
+      <div>
         <ul>
-            <li>
-                <NavLink to="/">Home</NavLink>
-            </li>
-             <li>
-                <NavLink to="signup">Sign Up</NavLink>
-            </li>
-            <li>
-                <NavLink to="login">Login</NavLink>
-            </li>
-            <li>
-                <NavLink to="logout">Log Out</NavLink>
-            </li>
-            <li>
-                <NavLink to="wishlist">Wishlist</NavLink>
-            </li>
-             <li>
-                <NavLink to="visited">Visited</NavLink>
-            </li>
-            <li>
-                <NavLink to="reviews/mine">My Reviews</NavLink>
-            </li>
-
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>{!account && <NavLink to="signup">Sign Up</NavLink>}</li>
+          <li>{!account && <NavLink to="login">Login</NavLink>}</li>
+          <li>
+            {account && <NavLink to="wishlist">Wishlist</NavLink>}
+            {!account && <NavLink to="login">Wishlist</NavLink>}
+          </li>
+          <li>
+            {account && <NavLink to="visited">Visited</NavLink>}
+            {!account && <NavLink to="login">Visited</NavLink>}
+          </li>
+          <li>
+            {account && <NavLink to="reviews/mine">My Reviews</NavLink>}
+            {!account && <NavLink to="login">My Reviews</NavLink>}
+          </li>
         </ul>
-        </div>
+        {account && <button onClick={logout}>Log Out</button>}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- ghi/src/app/apiSlice.js
+++ ghi/src/app/apiSlice.js
@@ -1,56 +1,56 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const npsApi = createApi({
-    reducerPath: 'npsApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://localhost:8000'
+  reducerPath: "npsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8000",
+  }),
+  endpoints: (builder) => ({
+    getAccount: builder.query({
+      query: () => ({
+        url: `/token`,
+        credentials: "include",
+      }),
+      transformResponse: (response) => (response ? response.account : null),
+      providesTags: ["Account"],
     }),
-    endpoints: (builder) => ({
-        getAccount: builder.query({
-            query: () => ({
-                url: `/token`,
-                credentials: 'include'
-            }),
-            transformResponse: (response) => response ? response.account: null,
-            providesTags: ['Account']
-        }),
-        logout: builder.mutation({
-            query: () => ({
-                url: '/token',
-                method: 'DELETE',
-                credentials: 'include'
-            }),
-            invalidatesTags: ['Account']
-        }),
-        login: builder.mutation({
-            query: ({username, password}) => {
-                const body = new FormData()
-                body.append('username', username)
-                body.append('password', password)
-                return {
-                    url: '/token',
-                    method: 'POST',
-                    body,
-                    credentials: 'include'
-                }
-            },
-            invalidatesTags: ['Account']
-        }),
-        signup: builder.mutation({
-            query: (body) => ({
-                url: '/api/accounts',
-                method: 'POST',
-                body,
-                credentials: 'include'
-            }),
-            invalidatesTags: ['Account']
-        })
-    })
-})
+    logout: builder.mutation({
+      query: () => ({
+        url: "/token",
+        method: "DELETE",
+        credentials: "include",
+      }),
+      invalidatesTags: ["Account"],
+    }),
+    login: builder.mutation({
+      query: ({ username, password }) => {
+        const body = new FormData();
+        body.append("username", username);
+        body.append("password", password);
+        return {
+          url: "/token",
+          method: "POST",
+          body,
+          credentials: "include",
+        };
+      },
+      invalidatesTags: ["Account"],
+    }),
+    signup: builder.mutation({
+      query: (body) => ({
+        url: "/api/accounts",
+        method: "POST",
+        body,
+        credentials: "include",
+      }),
+      invalidatesTags: ["Account"],
+    }),
+  }),
+});
 
 export const {
-    useSignupMutation,
-    useLoginMutation,
-    useLogoutMutation,
-    useGetAccountQuery
-} = npsApi
+  useSignupMutation,
+  useLoginMutation,
+  useLogoutMutation,
+  useGetAccountQuery,
+} = npsApi;
