1266bda0e23156750321eeecc15ba7d2a6c44356
SeanMcAuley <seanymac801@yahoo.com>
2023-07-12 16:27:19 -0600

    started work on the park detail page

--- ghi/src/ParkDetails.jsx
+++ ghi/src/ParkDetails.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import {useParams, Link } from "react-router-dom";
 
-const ParkDetails = () => {
+function ParkDetails() {
+  let { parkCode } = useParams();
+  const [ park, setPark] = useState(null);
+
+  useEffect(() => {
+    async function fetchPark() {
+      const response = await fetch(`http://localhost:8000/api/parks/code/${parkCode}`);
+
+      if (response.ok) {
+        const data = await response.json();
+
+        setPark(data[0]);
+      }
+    }
+
+    fetchPark();
+  }, [parkCode]);
+
+  if (!park) {
+    return <div>Loading...</div>;
+  }
   return (
-    <div>ParkDetails</div>
-  )
+    <div>
+      <h2>{park.fullName}</h2>
+      <div>{park.description}</div>
+      <img
+        src={park.images[0].url}
+        alt={park.fullName}
+        style={{ width: "400px", border: "4px solid #00b601" }}
+      />
+      <div>
+        <table>
+          <tbody className="border-top border-dark-subtle">
+          {park.activities.map((activity) => {
+            return (
+              <tr key={activity.id}>
+                <td>{activity.name}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default ParkDetails
