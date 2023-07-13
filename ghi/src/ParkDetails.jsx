28594f84431e039886ec650c372b194fd4c8b70a
Brad <bradcochi@gmail.com>
2023-07-13 13:00:38 -0600

    created park details page

--- ghi/src/ParkDetails.jsx
+++ ghi/src/ParkDetails.jsx
@@ -1,52 +1,79 @@
 import React, { useEffect, useState } from "react";
-import {useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-function ParkDetails() {
-  let { parkCode } = useParams();
-  const [ park, setPark] = useState(null);
+const ParkDetails = () => {
+  let { code } = useParams();
+
+  const [park, setPark] = useState();
+
+  const fetchData = async () => {
+    console.log({ code });
+    const url = `http://localhost:8000/api/parks/code/${code}`;
+
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      setPark(data.data[0]);
+      console.log(data.data[0]);
+    }
+  };
 
   useEffect(() => {
-    async function fetchPark() {
-      const response = await fetch(`http://localhost:8000/api/parks/code/${parkCode}`);
-
-      if (response.ok) {
-        const data = await response.json();
-
-        setPark(data[0]);
-      }
-    }
-
-    fetchPark();
-  }, [parkCode]);
+    fetchData();
+  }, []);
 
   if (!park) {
     return <div>Loading...</div>;
   }
+
   return (
     <div>
       <h2>{park.fullName}</h2>
       <div>{park.description}</div>
-      <img
-        src={park.images[0].url}
-        alt={park.fullName}
-        style={{ width: "400px", border: "4px solid #00b601" }}
-      />
+      {park.images.length > 0 && (
+        <img
+          src={park.images[0].url}
+          alt={park.fullName}
+          style={{
+            width: "300px",
+            height: "300px",
+            border: "4px solid #C09B75",
+          }}
+        />
+      )}
       <div>
         <table>
           <tbody className="border-top border-dark-subtle">
-          {park.activities.map((activity) => {
-            return (
-              <tr key={activity.id}>
-                <td>{activity.name}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-
+            {park.activities.map((activity) => {
+              return (
+                <tr key={activity.id}>
+                  <td>{activity.name}</td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     </div>
   );
-}
+};
 
-export default ParkDetails
+export default ParkDetails;
+
+// function ParkDetails() {
+//   let { parkCode } = useParams();
+//   const [ park, setPark] = useState(null);
+
+//   useEffect(() => {
+//     async function fetchPark() {
+//       const response = await fetch(`http://localhost:8000/api/parks/code/${parkCode}`);
+
+//       if (response.ok) {
+//         const data = await response.json();
+
+//         setPark(data[0]);
+//       }
+//     }
+
+//     fetchPark();
+//   }, [parkCode]);
--- ghi/src/ParksByStateList.jsx
+++ ghi/src/ParksByStateList.jsx
@@ -3,8 +3,7 @@ import "react-router-dom";
 import { useParams, Link } from "react-router-dom";
 
 const ParksByStateList = () => {
-
-  let { state } = useParams()
+  let { state } = useParams();
 
   const [parks, setParks] = useState([]);
 
@@ -15,7 +14,7 @@ const ParksByStateList = () => {
     if (response.ok) {
       const data = await response.json();
       setParks(data.data);
-      console.log(data)
+      console.log(data);
     }
   };
 
@@ -25,15 +24,18 @@ const ParksByStateList = () => {
   return (
     <>
       <table className="table table-hover table-secondary table-striped border border-dark-subtle shadow container-fluid mt-5">
-
         <tbody className="border-top border-dark-subtle">
           {parks.map((park) => {
             return (
               <tr key={park.id}>
-                <td><Link to={`/park/${park.parkCode}`}>{park.fullName}</Link></td>
+                <td>
+                  <Link to={`/park/${park.parkCode}`}>{park.fullName}</Link>
+                </td>
                 <td>{park.description}</td>
                 <td>
-                  { park.images.length > 0 && <img src={park.images[0].url} alt={park.fullName} /> }
+                  {park.images.length > 0 && (
+                    <img src={park.images[0].url} alt={park.fullName} />
+                  )}
                   {/* <img src={park.images[0].url} alt={park.fullName} /> */}
                 </td>
               </tr>
