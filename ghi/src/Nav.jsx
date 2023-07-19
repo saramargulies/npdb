4fec0dbacbf1e8aaed176f83c6bd7c04db32419e
Brad <bradcochi@gmail.com>
2023-07-19 17:32:26 -0600

    added logo linked to home in nav

--- ghi/src/Nav.jsx
+++ ghi/src/Nav.jsx
@@ -11,46 +11,100 @@ const Nav = () => {
   return (
     <nav className="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
       <div className="container-custom container">
-            <div className="navbar-brand">
-
-                <span className="navbar-caption-wrap"><a className="navbar-caption display-5" href="https://mobiri.se"><strong>Edge</strong><span><strong>M5</strong></span></a></span>
+        <div className="navbar-brand">
+          <span className="navbar-caption-wrap">
+            <div>
+              <NavLink to="/" className="nav-link link display-7">
+                <img src="NPDBlogo.png" alt="logo" />
+              </NavLink>
             </div>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <div className="hamburger">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
-                <div className="navbar-nav-container">
-                    <ul className="navbar-nav nav-dropdown" data-app-modern-menu="true">
-                        <li className="nav-item"><NavLink to="/" className="nav-link link display-7" >
-                            Home</NavLink></li>
-                        <li className="nav-item">{account && <NavLink to="wishlist" className="nav-link link display-7" >
-                            Wishlist</NavLink>} {!account && <NavLink to="login">Wishlist</NavLink>}</li>
-                        <li className="nav-item">{account && <NavLink to="visited" className="nav-link link display-7" >
-                            Visited</NavLink>} {!account && <NavLink to="login">Visited</NavLink>}</li>
-                        <li className="nav-item">{account && <NavLink to="reviews/mine" className="nav-link link display-7" >
-                            My Reviews</NavLink>} {!account && <NavLink to="login">My Reviews</NavLink>}</li>
-                        <li className="nav-item">{account && <NavLink to="/ws" className="nav-link link display-7" >
-                            Live Chat</NavLink>} {!account && <NavLink to="login">Live Chat</NavLink>}</li>
-                    </ul>
-                    {account && <button onClick={logout}>Log Out</button>}
-                </div>
-
-                <div className="navbar-nav-container">
-                    <ul className="navbar-nav nav-dropdown" data-app-modern-menu="true">
-                        <li className="nav-item">{!account && <NavLink to="signup" className="btn btn-primary display-7" >
-                            Sign Up</NavLink>}</li>
-                        <li className="nav-item">{!account && <NavLink to="login" className="btn btn-primary display-7" >
-                            Login</NavLink>}</li>
-                    </ul>
-                </div>
-            </div>
+          </span>
         </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-bs-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <div className="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="navbar-nav-container">
+            <ul className="navbar-nav nav-dropdown" data-app-modern-menu="true">
+              <li className="nav-item">
+                <NavLink to="/" className="nav-link link display-7">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                {account && (
+                  <NavLink to="wishlist" className="nav-link link display-7">
+                    Wishlist
+                  </NavLink>
+                )}{" "}
+                {!account && <NavLink to="login">Wishlist</NavLink>}
+              </li>
+              <li className="nav-item">
+                {account && (
+                  <NavLink to="visited" className="nav-link link display-7">
+                    Visited
+                  </NavLink>
+                )}{" "}
+                {!account && <NavLink to="login">Visited</NavLink>}
+              </li>
+              <li className="nav-item">
+                {account && (
+                  <NavLink
+                    to="reviews/mine"
+                    className="nav-link link display-7"
+                  >
+                    My Reviews
+                  </NavLink>
+                )}{" "}
+                {!account && <NavLink to="login">My Reviews</NavLink>}
+              </li>
+              <li className="nav-item">
+                {account && (
+                  <NavLink to="/ws" className="nav-link link display-7">
+                    Live Chat
+                  </NavLink>
+                )}{" "}
+                {!account && <NavLink to="login">Live Chat</NavLink>}
+              </li>
+            </ul>
+          </div>
+          {account && <button onClick={logout}>Log Out</button>}
+
+          <div className="navbar-nav-container">
+            <ul className="navbar-nav nav-dropdown" data-app-modern-menu="true">
+              <li className="nav-item">
+                {!account && (
+                  <NavLink to="signup" className="btn btn-primary display-7">
+                    Sign Up
+                  </NavLink>
+                )}
+              </li>
+              <li className="nav-item">
+                {!account && (
+                  <NavLink to="login" className="btn btn-primary display-7">
+                    Login
+                  </NavLink>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
       {/* <div>
         <ul>
           <li>
