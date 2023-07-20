a9501d831845b6acf2e8689400fef4413ea5c4eb
Ryan Wade <ryanwade@Ryans-MBP.lan>
2023-07-20 14:47:29 -0500

    footer and footer css added

---
+++ ghi/src/Footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './footer.css'; 
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="column">
+          <h4>
+            <img src="NPSlogo.png" alt="NPS logo" className="image" />
+          </h4>
+          <ul className="list">
+            <li>This website was made using</li>
+            <li>the National Park Service Api at</li>
+            <li>
+              <a
+                href="https://www.nps.gov/subjects/developer/index.htm"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                https://www.nps.gov/index.htm/
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <div className="bottom">
+        <p>&copy; {new Date().getFullYear()} National Parks Database</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
