b5ebef713d91c52d8daff972cdfcd4250b943aa4
Sara <saraaustenm@gmail.com>
2023-07-20 15:01:23 -0600

    styled the park list page

--- .history/ghi/src/ParkDetails_20230720145641.jsx
+++ .history/ghi/src/ParkDetails_20230720145641.jsx
@@ -52,7 +52,6 @@ const ParkDetails = () => {
       }
     }
   }
-  console.log(park)
 
   let parkProps = {
     "parkCode": parkCode,
@@ -74,7 +73,7 @@ const ParkDetails = () => {
       <div>{park.description}</div>
       {park.images.length > 0 && (
         <img
-          src={park.images[1]?.url}
+          src={park?.images[1].url}
           alt={park.fullName}
         />
       )}
---
+++ .history/ghi/src/ParksByStateList_20230720142319.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+
+const ParksByStateList = () => {
+  let { state } = useParams();
+
+  const [parks, setParks] = useState([]);
+
+  const fetchData = async () => {
+    const url = `http://localhost:8000/api/parks/${state}`;
+
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      setParks(data.data);
+      console.log(data);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <>
+      <table>
+        <tbody>
+          {parks.map((park) => {
+            return (
+              <tr key={park.id}>
+                <td>
+                  
+                  <Link to={`/park/${park.parkCode}`}>{park.fullName}</Link>
+
+
+                </td>
+                <td>{park.description}</td>
+                <td>
+                  {park.images.length > 0 && (
+                    <img src={park.images[0].url} alt={park.fullName} />
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default ParksByStateList;
