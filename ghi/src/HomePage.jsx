d61c472af53f05910828d1f197228cd747779934
bradcochi <bradcochi@gmail.com>
2023-07-21 21:27:35 +0000

    Map styling

--- ghi/src/HomePage.jsx
+++ ghi/src/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import USAMap from "react-usa-map";
+import "./map.css";
 
 const HomePage = () => {
   let navigate = useNavigate();
